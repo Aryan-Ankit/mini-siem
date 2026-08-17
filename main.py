b34504715685d35147e83a518287cea58cf439a6
Aryan Ankit <aryanankit2006@gmail.com>
2026-08-18 02:26:49 +0530

    Add timestamp correlation for failed logins

--- main.py
+++ main.py
@@ -49,31 +49,40 @@ events = [
    },
 
    {
-          "timestamp": "10:00:08",
+          "timestamp": "10:00:12",
           "username": "Bob",
           "event_type": "login_failed",
           "source_ip": "192.168.1.30",
     },
 
     {
-           "timestamp": "10:00:08",
+           "timestamp": "10:00:18",
            "username": "Charlie",
            "event_type": "login_failed",
            "source_ip": "192.168.1.50",
     }
 ]
 
+from datetime import datetime
+
 failed_attempts = {}
 
-for event in events:
+for event in events: 
     if event["event_type"] == "login_failed":
 
         username = event["username"]
 
         if username not in failed_attempts:
-           failed_attempts[username].append(event["timestamp"]) = 1
+            failed_attempts[username] = []
+            failed_attempts[username].append(event["timestamp"])
+        else:
+            failed_attempts[username].append(event["timestamp"])
 
-        else :
-           failed_attempts[username].append(event["timestamp"]) += 1
-        
-print(failed_attempts)
+for username, timestamp in failed_attempts.items():
+
+ first_time = datetime.strptime(timestamp[0], "%H:%M:%S")
+ last_time = datetime.strptime(timestamp[-1], "%H:%M:%S")
+
+ difference = last_time - first_time
+
+ print(username, difference)
